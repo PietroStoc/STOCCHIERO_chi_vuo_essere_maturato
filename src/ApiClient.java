60ee2cb412ba33055565b7200887fa1b149e394c
pstoc <10934128@itisrossi.vi.it>
2025-09-26 08:37:59 +0200

    Arrivato fino a punto 5

--- src/ApiClient.java
+++ src/ApiClient.java
@@ -3,13 +3,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import com.google.gson.Gson;
 
 public class ApiClient {
+
     private final HttpClient client = HttpClient.newHttpClient();
 
     public String fetchQuestions(int amount, String difficulty, String type) {
 
-        String url = "https://opentdb.com/api.php?amount=5";
+        String url = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -25,6 +27,16 @@ public class ApiClient {
         if (resp == null) {
             throw new RuntimeException("No response received from API");
         }
+
+        Gson gson = new Gson();
+
+        ApiResponse response = gson.fromJson(resp.body(), ApiResponse.class);
+
+        for (ApiQuestion q : response.results) {
+            System.out.println(q.question);
+            System.out.println("Risposta corretta : " + q.correct_answer);
+        }
+
         return resp.body();
-    }
+        }
 }
